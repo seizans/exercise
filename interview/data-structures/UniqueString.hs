55c90f2e22e569894fcb4e3191fa4c115a51dfa7
seizans <seizans@gmail.com>
2013-01-04 10:35:29 +0900

    UniqueString check

---
+++ interview/data-structures/UniqueString.hs
@@ -0,0 +1,15 @@
+import Control.Applicative
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+    str <- head <$> getArgs
+    print $ isAllUnique str
+
+isAllUnique :: String -> Bool
+isAllUnique [] = True
+isAllUnique (c:cs) = if c `elem` cs then False else isAllUnique cs
+
+-- below is substituted by elem
+-- contains :: Char -> String -> Bool
+-- contains c = any (==c)
